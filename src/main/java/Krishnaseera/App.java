8228d2b29b1837a2e2244d048ff9d467f9a354ba
root <root@ip-172-31-9-3.us-west-2.compute.internal>
2022-08-19 16:57:24 +0000

    projects got created

---
+++ src/main/java/Krishnaseera/App.java
@@ -0,0 +1,13 @@
+package Krishnaseera;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
